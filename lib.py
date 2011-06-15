8c161264ed2dcc566582dc3476e9ce77c7c3894a
Callum Macrae <callumacrae95@gmail.com>
2011-06-15 11:16:31 +0100

    Moved stuff to their own files.

---
+++ lib.py
@@ -0,0 +1,53 @@
+if __name__ == '__main__':
+	print('This is a library. Get out.')
+	exit()
+
+nums = {
+	'quarter': 0.25,
+	'half': 0.5,
+	'one': 1,
+	'two': 2,
+	'three': 3,
+	'thir': 3,
+	'four': 4,
+	'five': 5,
+	'fif': 5,
+	'six': 6,
+	'seven': 7,
+	'eight': 8,
+	'eigh': 8,
+	'nine': 9,
+	'ten': 10,
+	'eleven': 11,
+	'twelve': 12,
+	'twenty': 20,
+	'hundred': 100,
+	'thousand': 1000,
+	'million': 1000000
+};
+
+def parse(string):
+	if (string in nums):
+		return nums[string]
+
+	if (string.find(' ')  == -1): #single word
+		if (string.replace('ty', '') in nums):
+			return nums[string.replace('ty', '')] * 10
+
+		if (string.replace('teen', '') in nums):
+			return nums[string.replace('teen', '')] + 10
+
+	return -1
+
+def get(string):
+	try:
+		float(string)
+		return 'That\'s already an int!'
+	except(ValueError):
+		cont = 1
+
+	parsed_string = parse(string)
+	if (parsed_string == -1):
+		return 'Failed to parse string.'
+	else:
+		return '"' + string + '" parses to ' + str(parsed_string)
--- numbers.py
+++ numbers.py
@@ -1,58 +1,9 @@
-import os, sys
+import os, lib, sys
+from lib import get
 
 if sys.version_info[0] < 3:
 	print('This application was designed to run for Python3. Get out')
 	exit()
-
-nums = {
-	'quarter': 0.25,
-	'half': 0.5,
-	'one': 1,
-	'two': 2,
-	'three': 3,
-	'thir': 3,
-	'four': 4,
-	'five': 5,
-	'fif': 5,
-	'six': 6,
-	'seven': 7,
-	'eight': 8,
-	'eigh': 8,
-	'nine': 9,
-	'ten': 10,
-	'eleven': 11,
-	'twelve': 12,
-	'twenty': 20,
-	'hundred': 100,
-	'thousand': 1000,
-	'million': 1000000
-};
-
-def parse(string):
-	if (string in nums):
-		return nums[string]
-
-	if (string.find(' ')  == -1): #single word
-		if (string.replace('ty', '') in nums):
-			return nums[string.replace('ty', '')] * 10
-
-		if (string.replace('teen', '') in nums):
-			return nums[string.replace('teen', '')] + 10
-
-	return -1
-
-def get(string):
-	try:
-		float(string)
-		return 'That\'s already an int!'
-	except(ValueError):
-		cont = 1
-
-	parsed_string = parse(string)
-	if (parsed_string == -1):
-		return 'Failed to parse string.'
-	else:
-		return '"' + string + '" parses to ' + str(parsed_string)
 
 #if specified in argv, print and exit
 if (len(sys.argv) > 1):
